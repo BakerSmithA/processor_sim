fad5c4c5aa1b64e00e426ab9eccc6ea44224cbc7
Albie Baker-Smith <bakersmitha@gmail.com>
2018-10-11 08:59:54 +0100

    wordI parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,6 +3,7 @@ module Parser where
 import Data.Word
 import Data.ByteString
 import Data.Bits
+import Instr
 
 newtype Parser a = Parser { runParser :: ByteString -> Maybe (a, ByteString) }
 
@@ -28,6 +29,10 @@ instance Monad Parser where
         let Parser px' = f x
         px' bs'
 
+failure :: Parser a
+failure = Parser $ \bs -> Nothing
+
+-- Parses byte from start of string.
 word8 :: Parser Word8
 word8 = Parser $ \bs -> do
     (h, t) <- uncons bs
@@ -37,8 +42,20 @@ fromOctets :: ByteString -> Word32
 fromOctets = foldl' accum 0 where
     accum a b = (a `shiftL` 8) .|. fromIntegral b
 
+-- Parses 4 bytes from string into Word32.
 word32 :: Parser Word32
 word32 = Parser $ \bs -> do
     let octets = Data.ByteString.take 4 bs
         rest   = Data.ByteString.drop 4 bs
     return (fromOctets octets, rest)
+
+-- Parses if the byte at the start of the string is the supplied byte, otherwise fails.
+byte :: Word8 -> Parser Word8
+byte w = do
+    actual <- word8
+    if w == actual
+        then return w
+        else failure
+
+wordI :: Parser Instr
+wordI = MoveI <$ byte 0 <*> word32 <*> word32
