e227f48efd8af8dc7699b847e495694fe5d2c9ea
Albie Baker-Smith <bakersmitha@gmail.com>
2018-10-18 23:19:20 +0100

    Added inc

--- src/VM.hs
+++ src/VM.hs
@@ -9,11 +9,13 @@ import qualified Mem as Reg
 -- Current state of the virtual machine, or whether it crashed, e.g. by
 -- accessing memory index that is out of bounds.
 data VM a = VM a
+          | End
           | Crash Error
 
 instance Functor VM where
     -- fmap :: (a -> b) -> f a -> f b
     fmap f (VM x)    = VM (f x)
+    fmap f (End)     = End
     fmap _ (Crash e) = Crash e
 
 instance Applicative VM where
@@ -21,11 +23,13 @@ instance Applicative VM where
     pure = VM
     -- (<*>) :: f (a -> b) -> f a -> f b
     (VM f)    <*> vm = fmap f vm
+    (End)     <*> _ = End
     (Crash e) <*> _ = Crash e
 
 instance Monad VM where
     -- (>>=) :: m a -> (a -> m b) -> m b
     (VM x)    >>= f  = f x
+    (End)     >>= _ = End
     (Crash e) >>= _ = Crash e
 
 -- Return value of a register, or Crash if invalid index.
@@ -56,14 +60,22 @@ setMemVal i val st =
         Nothing  -> Crash (MemOutOfRange i)
         Just mem -> return st { mem = mem }
 
+-- Increments the PC by 1, or returns End if at the last instruction.
+inc :: State -> VM State
+inc st = do
+    pc <- regVal (pcIdx st) st
+    if pc > Mem.maxAddr (instrs st)
+        then End
+        else setRegVal (pcIdx st) (pc + 1) st
+
 -- Loads contents of memory at address into register.
-load :: Addr -> RegIdx ->State -> VM State
+load :: Addr -> RegIdx -> State -> VM State
 load addr r st = do
     val <- regVal r st
-    setMemVal addr val st
+    setMemVal addr val st >>= inc
 
 -- Stores contents of register into memory address.
 store :: RegIdx -> Addr -> State -> VM State
 store r addr st = do
     val <- memVal addr st
-    setRegVal r val st
+    setRegVal r val st >>= inc
