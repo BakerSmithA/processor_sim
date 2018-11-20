8beda13a33b368dd1427972a57818185f0c945a6
Albie Baker-Smith <bakersmitha@gmail.com>
2018-11-20 21:29:46 +0000

    docs

--- src/RS.hs
+++ src/RS.hs
@@ -3,16 +3,17 @@ module RS where
 import Instr (Addr, RegIdx, Val)
 import Bypass (Bypass(..))
 
--- The location an operation is waiting for data from.
-data Src = Mem Addr
+-- Placeholder for operation that requires operands to be filled in.
+-- Details the location of where the operand will come from.
+data Tag = Mem Addr
          | Reg RegIdx
          deriving (Eq, Show)
 
 -- Entry in reservation station. Details operation that is waiting for operands.
-data Waiting = WaitingU Src     -- Unary operation waiting for single operand.
-             | WaitingL Src Val -- Binary operation waiting for left operand, and right has already been supplied.
-             | WaitingR Val Src -- Binary operation waiting for right operand, and left has already been supplied.
-             | WaitingB Src Src -- Binary operation waiting for both operands.
+data Waiting = WaitingU Tag     -- Unary operation waiting for single operand.
+             | WaitingL Tag Val -- Binary operation waiting for left operand, and right has already been supplied.
+             | WaitingR Val Tag -- Binary operation waiting for right operand, and left has already been supplied.
+             | WaitingB Tag Tag -- Binary operation waiting for both operands.
              deriving (Eq, Show)
 
 -- Operation that has had all operands 'filled in' and is ready to be performed.
@@ -46,25 +47,25 @@ fill b (RS ws) = foldr f ([], empty) ws where
 
 -- Attempt to 'fill in' value in operation that is waiting for operands.
 fillOp :: Waiting -> Bypass -> Either Waiting FilledOp
-fillOp w@(WaitingU src)    b = maybe (Left w) (\v -> Right $ UniOp v) (checkSrc src b)
-fillOp w@(WaitingL src vr) b = maybe (Left w) (\vl -> Right $ BinOp vl vr) (checkSrc src b)
-fillOp w@(WaitingR vl src) b = maybe (Left w) (\vr -> Right $ BinOp vl vr) (checkSrc src b)
+fillOp w@(WaitingU src)    b = maybe (Left w) (\v -> Right $ UniOp v) (checkTag src b)
+fillOp w@(WaitingL src vr) b = maybe (Left w) (\vl -> Right $ BinOp vl vr) (checkTag src b)
+fillOp w@(WaitingR vl src) b = maybe (Left w) (\vr -> Right $ BinOp vl vr) (checkTag src b)
 fillOp w@(WaitingB sl sr)  b =
     -- Check whether left value can be filled in.
-    case (checkSrc sl b) of
+    case (checkTag sl b) of
         Just vl -> fillOp (WaitingR vl sr) b
         Nothing ->
             -- Check whether right value can be filled in.
-            case (checkSrc sr b) of
+            case (checkTag sr b) of
                 Just vr -> fillOp (WaitingL sl vr) b
                 Nothing -> Left w
 
 -- Check whether the source for an operand, that is being waiting, for can be
 -- fullfilled by the value passed via common data bus.
-checkSrc :: Src -> Bypass -> Maybe Val
-checkSrc (Mem addr) (BypassMem ckAddr val) = check addr ckAddr val
-checkSrc (Reg idx)  (BypassReg ckIdx  val) = check idx ckIdx val
-checkSrc _ _ = Nothing
+checkTag :: Tag -> Bypass -> Maybe Val
+checkTag (Mem addr) (BypassMem ckAddr val) = check addr ckAddr val
+checkTag (Reg idx)  (BypassReg ckIdx  val) = check idx ckIdx val
+checkTag _ _ = Nothing
 
 check :: (Eq a) => a -> a -> b -> Maybe b
 check x y v | x == y = Just v
