21d0fb481a01cb18827c78700d7e61dc2170f264
Albie Baker-Smith <bakersmitha@gmail.com>
2018-11-20 23:08:03 +0000

    stubbed units

--- src/ExecUnit.hs
+++ src/ExecUnit.hs
@@ -2,8 +2,23 @@ module ExecUnit where
 
 import Instr
 import RS
+import Pipeline
+import State
 
 type Occupied = Bool
 
 -- Used to represent ALU, Load/Store Unit, etc
-data ExecUnit = ExecUnit ((Instr, FilledOp) -> a) Occupied
+data ExecUnit a = ExecUnit (Instr -> FilledOp -> State -> a) Occupied
+
+-- Create execution unit that is unoccupied.
+unit :: (Instr -> FilledOp -> State -> a) -> ExecUnit a
+unit f = ExecUnit f False
+
+arithLogicUnit :: ExecUnit WriteBackInstr
+arithLogicUnit = undefined
+
+loadStoreUnit :: ExecUnit WriteBackInstr
+loadStoreUnit = undefined
+
+branchUnit :: ExecUnit WriteBackInstr
+branchUnit = undefined
