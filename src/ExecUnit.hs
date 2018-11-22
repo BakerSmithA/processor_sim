45f000ebd74f9e95ceea8c23773063f3a4a396a5
Albie Baker-Smith <bakersmitha@gmail.com>
2018-11-22 15:19:41 +0000

    stubbed ls unit

--- src/ExecUnit.hs
+++ src/ExecUnit.hs
@@ -14,15 +14,19 @@ data ExecUnit a = ExecUnit (Instr -> FilledOp -> State -> a) Occupied
 unit :: (Instr -> FilledOp -> State -> a) -> ExecUnit a
 unit f = ExecUnit f False
 
-arithLogicUnit :: ExecUnit WriteBackInstr
-arithLogicUnit = unit al where
-    al (MoveI r _) (UniOp x)   _ = WriteReg r x
-    al (Move r _)  (UniOp x)   _ = WriteReg r x
-    al (Add r _ _) (BinOp x y) _ = WriteReg r (x + y)
-
 loadStoreUnit :: ExecUnit WriteBackInstr
 loadStoreUnit = unit ls where
-    ls = undefined
+    ls (MoveI r i)          (EmptyOp)     _ = WriteReg r i
+    ls (Move r _)           (UniOp x)     _ = WriteReg r x
+    ls (LoadIdx r _ off)    (UniOp base)  _ = undefined
+    ls (LoadBaseIdx r _ _)  (BinOp b off) _ = undefined
+    ls (StoreIdx r _ off)   (UniOp base)  _ = undefined
+    ls (StoreBaseIdx r _ _) (BinOp b off) _ = undefined
+
+arithLogicUnit :: ExecUnit WriteBackInstr
+arithLogicUnit = unit al where
+    al (Add r _ _)  (BinOp x y) _ = WriteReg r (x + y)
+    al (AddI r _ i) (UniOp x)   _ = WriteReg r (x + i)
 
 branchUnit :: ExecUnit WriteBackInstr
 branchUnit = unit b where
