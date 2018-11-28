951e2e64b7a874cc952b2293f1f2ca10acb65f02
Albie Baker-Smith <bakersmitha@gmail.com>
2018-11-28 17:19:16 +0000

    ROB struct

--- src/ROB.hs
+++ src/ROB.hs
@@ -4,5 +4,9 @@ import Queue (Queue)
 import qualified Queue as Q
 import WriteBack
 
+-- Entry into the Reorder Buffer. Whether the instruction is present determines
+-- whether the entry is ready to be committed.
+data Entry = Entry (Maybe WriteBack)
+
 -- Reorder Buffer, used to store write-back instructions before they are committed.
-data ROB = ROB Queue
+data ROB = ROB (Queue Entry)
