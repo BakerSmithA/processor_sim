4407db56485c61af080bd6a380b2d412d3f4051e
Albie Baker-Smith <bakersmitha@gmail.com>
2018-11-29 10:04:07 +0000

    pipeline

--- src/Pipeline.hs
+++ src/Pipeline.hs
@@ -12,26 +12,32 @@ data Pipeline = Pipeline {
   , committed :: Maybe [WriteBack]
 } deriving (Show)
 
+-- Decodes a fetched instruction.
+type Decoder m = Instr -> m Instr
+-- Executes a decoded instruction.
+type Executer m = Instr -> m WriteBack
+-- Commits any executed instructions, and returns instructions that can be committed.
+type Committer m a = (ROBIdx, WriteBack) -> m ([WriteBack], a)
+-- Writes instructions results to memory/registers.
+type Writer  m a = ([WriteBack], a) -> m a
+
 -- Return pipeline with nothing in each stage.
 empty :: Pipeline
 empty = Pipeline Nothing Nothing Nothing Nothing
 
-type Decoder   m   = Instr               -> m Instr
-type Executer  m   = Instr               -> m WriteBack
-type Committer m a = (ROBIdx, WriteBack) -> m ([WriteBack], a)
-type Writer    m a = ([WriteBack], a)    -> m a
+-- Performs a step of the pipeline.
+step :: (Monad m) => (a -> m b) -> Maybe a -> m (Maybe b)
+step f = maybe (return Nothing) success where
+    success x = do
+        y <- f x
+        return (Just y)
 
-decodeFetched :: Maybe (ROBIdx, Instr) -> Decoder m -> m (Maybe (ROBIdx, Instr))
-decodeFetched = undefined
-
-execDecoded :: Maybe (ROBIdx, Instr) -> Executer m -> m (Maybe (ROBIdx, WriteBack))
-execDecoded = undefined
-
-commitExeced :: Maybe (ROBIdx, WriteBack) -> Committer m a -> m (Maybe ([WriteBack], a))
-commitExeced = undefined
-
-wbComitted :: Maybe ([WriteBack], a) -> Writer m a -> m (Maybe a)
-wbComitted = undefined
+-- Performs a step of the pipeline, passing through an acompanying ROBIdx.
+stepPassROB :: (Monad m) => (a -> m b) -> Maybe (ROBIdx, a) -> m (Maybe (ROBIdx, b))
+stepPassROB f pre = maybe (return Nothing) success pre where
+    success (idx, x) = do
+        post <- f x
+        return (Just (idx, post))
 
 -- Supplies new instruction into pipleine, and shifts in-flight instructions
 -- through pipeline. Returns write-back result, and new state of pipeline.
@@ -45,35 +51,8 @@ advance :: (Monad m) => Maybe (ROBIdx, Instr)
 
 advance newFetched decode exec commit write p = do
     -- Decode the previously fetched instruction to obtain the currently decoded instruction.
-    newDecoded  <- decodeFetched (fetched p) decode
-    newExecuted <- execDecoded   (decoded p) exec
-    newComitted <- commitExeced  (executed p) commit
-    newWritten  <- wbComitted    newComitted write
+    newDecoded  <- stepPassROB decode (fetched p)
+    newExecuted <- stepPassROB exec   (decoded p)
+    newComitted <- step        commit (executed p)
+    newWritten  <- step        write  newComitted
     return (newWritten, Pipeline newFetched newDecoded newExecuted (fmap fst newComitted))
-
--- -- Helper for advance,
--- op :: (Monad m) => (a -> m b) -> Maybe a ->  m (Maybe b)
--- op f = maybe (return Nothing) (fmap Just . f)
---
--- -- Helper for advance which passes through the ROBIdx.
--- opRob :: (Monad m) => (a -> m b) -> Maybe (ROBIdx, a) ->  m (Maybe (ROBIdx, b))
--- opRob f = maybe failure success where
---     success (idx, x) = fmap (\x' -> Just (idx, x')) (f x)
---     failure          = return Nothing
---
--- -- Supplies new instruction into pipleine, and shifts in-flight instructions
--- -- through pipeline. Returns write-back result, and new state of pipeline.
--- advance :: (Monad m) => Maybe (ROBIdx, Instr)
---                      -> Decoder m
---                      -> Executer m
---                      -> Commiter m a
---                      -> Writer m a
---                      -> Pipeline
---                      -> m (Maybe a, Pipeline)
---
--- advance fetch decode exec commit write p = do
---     d <- opRob decode (fetched p)
---     e <- opRob exec (decoded p)
---     c <- op commit (executed p)
---     w <- write _
---     return (w, Pipeline fetch d e)
