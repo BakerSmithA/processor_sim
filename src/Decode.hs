a27f68b54f4389158f055cf5a0c9152b16694a2d
Albie Baker-Smith <bakersmitha@gmail.com>
2018-11-30 21:52:16 +0000

    started decode

--- src/Decode.hs
+++ src/Decode.hs
@@ -1,9 +1,20 @@
 module Decode where
 
 import Instr
-import State
+import State as St
+
+-- Because instruction are already parsed into struct, no need to decode.
+-- However, register renaming will be performed at this step.
+-- decode :: FInstr -> State -> Res (Maybe DInstr, State)
+-- decode i st = return (Just i, st)
 
 -- Because instruction are already parsed into struct, no need to decode.
 -- However, register renaming will be performed at this step.
 decode :: FInstr -> State -> Res (Maybe DInstr, State)
-decode i st = return (Just i, st)
+decode fi st = return $ maybe (Nothing, st) just (decodeI fi st) where
+    just (di, st') = (Just di, st')
+
+decodeI :: FInstr -> State -> Maybe (DInstr, State)
+decodeI (MoveI r v) st1 = do
+    (p, st2) <- St.allocPhyReg r st1
+    return (MoveI p v, st2)
--- src/Instr.hs
+++ src/Instr.hs
@@ -10,33 +10,33 @@ type Val = Int32
 
 data TemplateInstr reg addr
     -- Memory
-    = MoveI        { r :: reg, val :: Val }                        -- r <- val
-    | Move         { r :: reg, from :: reg }                    -- r <- [from]
-    | LoadIdx      { r :: reg, base :: reg, offset :: Val }     -- r <- [[base] + offset]
+    = MoveI        { r :: reg, val :: Val }                  -- r <- val
+    | Move         { r :: reg, from :: reg }                 -- r <- [from]
+    | LoadIdx      { r :: reg, base :: reg, offset :: Val }  -- r <- [[base] + offset]
     | LoadBaseIdx  { r :: reg, base :: reg, rOffset :: reg } -- r <- [[base] + [R_offset]]
-    | StoreIdx     { r :: reg, base :: reg, offset :: Val }     -- r -> [[base] + offset]
+    | StoreIdx     { r :: reg, base :: reg, offset :: Val }  -- r -> [[base] + offset]
     | StoreBaseIdx { r :: reg, base :: reg, rOffset :: reg } -- r -> [[base] + [R_offset]]
     -- Arithmetic/Logic
     | Add  { r :: reg, x :: reg, y :: reg } -- r <- [x] + [y]
-    | AddI { r :: reg, x :: reg, i :: Val }    -- r <- [x] + i
+    | AddI { r :: reg, x :: reg, i :: Val } -- r <- [x] + i
     | Sub  { r :: reg, x :: reg, y :: reg } -- r <- [x] - [y]
-    | SubI { r :: reg, x :: reg, i :: Val }    -- r <- [x] - i
+    | SubI { r :: reg, x :: reg, i :: Val } -- r <- [x] - i
     | Mult { r :: reg, x :: reg, y :: reg } -- r <- [x] * [y]
     | Div  { r :: reg, x :: reg, y :: reg } -- r <- [x] / [y]
     | Eq   { r :: reg, x :: reg, y :: reg } -- r <- [x] == [y]
     | Lt   { r :: reg, x :: reg, y :: reg } -- r <- [x] < [y]
     | Or   { r :: reg, x :: reg, y :: reg } -- r <- [x] || [y]
     | And  { r :: reg, x :: reg, y :: reg } -- r <- [x] && [y]
-    | Not  { r :: reg, x :: reg }              -- r <- ![x]
+    | Not  { r :: reg, x :: reg }           -- r <- ![x]
     -- Branching
-    | B  { addr :: addr }                 -- Unconditional branch to addr
+    | B  { addr :: addr }              -- Unconditional branch to addr
     | BT { r    :: reg, addr :: addr } -- Branch to addr if r == 1
     | BF { r    :: reg, addr :: addr } -- Branch to addr if r == 0
-    | Ret                                 -- Branch to address in link register.
-    | SysCall                             -- Terminates the program.
+    | Ret                              -- Branch to address in link register.
+    | SysCall                          -- Terminates the program.
     -- Debugging
     | Print { r :: reg } -- Print value in a register.
-    | PrintLn               -- Print a newline.
+    | PrintLn            -- Print a newline.
     deriving (Eq, Show)
 
 type FInstr = TemplateInstr RegIdx Addr
