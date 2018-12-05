65b506f0453b8926c123a4d87438ca63440a1c45
Albie Baker-Smith <bakersmitha@gmail.com>
2018-12-05 18:03:36 +0000

    stubbed rs promote

--- src/RS.hs
+++ src/RS.hs
@@ -23,3 +23,21 @@ tryFill st = mapM fill where
             return $ case maybeVal of
                 Nothing  -> Left phy
                 Just val -> Right val
+
+-- Removes instructions that have had all operands 'fill in'.
+-- Performs additional check given, and only promotes if returns true.
+promote :: (EInstr -> Bool) -> RS -> ([EInstr], RS)
+promote cond = foldr checkDone ([], []) where
+    checkDone :: RSInstr -> ([EInstr], RS) -> ([EInstr], RS)
+    checkDone rsInstr (execIs, rs) =
+        case rsToExec rsInstr of
+            Nothing        -> (execIs, rsInstr:rs)
+            Just execInstr -> if cond execInstr 
+                                  then (execInstr:execIs, rs)
+                                  else (execIs, rsInstr:rs)
+
+-- Attempts to convert an instruction stored in a reservation station to an
+-- executed instruction. Can occur if the RS instruction has had all operands
+-- filled in.
+rsToExec :: RSInstr -> Maybe EInstr
+rsToExec = undefined
