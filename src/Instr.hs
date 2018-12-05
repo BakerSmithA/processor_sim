197a58cdf3bfb0e3511734d1e6b3e4b31b824c3b
Albie Baker-Smith <bakersmitha@gmail.com>
2018-12-05 17:00:30 +0000

    instruction map

--- src/Instr.hs
+++ src/Instr.hs
@@ -46,10 +46,40 @@ type FInstr = TemplateInstr RegIdx RegIdx Addr
 type DInstr = TemplateInstr PhyReg PhyReg Addr
 -- Instruction stored in reservation station.
 -- Stores partially 'filled-in' instrucions.
--- type RSInstr = TemplateInstr (Either PhyReg Val) (Either Addr Val)
---
--- -- Map register and address values stored in instruction.
--- mapI :: (r1 -> r2) -> (a1 -> a2) -> TemplateInstr r1 a1 -> TemplateInstr r2 a2
+type RSInstr = TemplateInstr PhyReg (Either PhyReg Val) (Either Addr Val)
+
+-- Map register and address values stored in instruction.
+mapI :: (rDst1 -> rDst2) -> (rSrc1 -> rSrc2) -> (a1 -> a2) -> TemplateInstr rDst1 rSrc1 a1 -> TemplateInstr rDst2 rSrc2 a2
+-- Memory
+mapI fd _  _ (MoveI r v)            = MoveI (fd r) v
+mapI fd fs _ (Move r from)          = Move (fd r) (fs from)
+mapI fd fs _ (LoadIdx r b off)      = LoadIdx (fd r) (fs b) off
+mapI fd fs _ (LoadBaseIdx r b off)  = LoadBaseIdx (fd r) (fs b) (fs off)
+mapI _  fs _ (StoreIdx r b off)     = StoreIdx (fs r) (fs b) off
+mapI _  fs _ (StoreBaseIdx r b off) = StoreBaseIdx (fs r) (fs b) (fs off)
+-- Arithmetic/Logic
+mapI fd fs _ (Add  r x y) = Add  (fd r) (fs x) (fs y)
+mapI fd fs _ (AddI r x i) = AddI (fd r) (fs x) i
+mapI fd fs _ (Sub  r x y) = Sub  (fd r) (fs x) (fs y)
+mapI fd fs _ (SubI r x i) = SubI (fd r) (fs x) i
+mapI fd fs _ (Mult r x y) = Mult (fd r) (fs x) (fs y)
+mapI fd fs _ (Div  r x y) = Div  (fd r) (fs x) (fs y)
+mapI fd fs _ (Eq   r x y) = Eq   (fd r) (fs x) (fs y)
+mapI fd fs _ (Lt   r x y) = Lt   (fd r) (fs x) (fs y)
+mapI fd fs _ (Or   r x y) = Or   (fd r) (fs x) (fs y)
+mapI fd fs _ (And  r x y) = And  (fd r) (fs x) (fs y)
+mapI fd fs _ (Not  r x)   = Not  (fd r) (fs x)
+-- Branching
+mapI _  _ fa (B addr)    = B  (fa addr)
+mapI _ fs fa (BT r addr) = BT (fs r) (fa addr)
+mapI _ fs fa (BF r addr) = BF (fs r) (fa addr)
+mapI _ _  _  (Ret)       = Ret
+mapI _ _  _  (SysCall)   = SysCall
+-- Debugging
+mapI _ fs _ (Print  r) = Print  (fs r)
+mapI _ fs _ (PrintC r) = PrintC (fs r)
+mapI _ _  _ (PrintLn)  = PrintLn
+
 -- -- Memory.
 -- mapI fr _ (MoveI r v)            = MoveI        (fr r) v
 -- mapI fr _ (Move r from)          = Move         (fr r) (fr from)
