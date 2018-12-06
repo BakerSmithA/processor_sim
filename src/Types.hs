80995ceaf351c3d9b35c1792410816ff0c53d5bb
Albie Baker-Smith <bakersmitha@gmail.com>
2018-12-06 10:39:50 +0000

    added Types

---
+++ src/Types.hs
@@ -0,0 +1,11 @@
+module Types where
+
+import Data.Int (Int32)
+import Data.Word (Word8, Word32)
+
+type RegIdx = Word8
+type PhyReg = Int
+type Addr = Word32
+type Val = Int32
+type InstrAddr = Val
+type ROBIdx = Int
